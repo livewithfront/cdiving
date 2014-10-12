f9c88a87cd8d6d67666ab3ce43ada863b00641a8
livewithfront <312129003@qq.com>
2014-10-12 08:38:57 +0800

    测试新增文件，然后push

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>
+		</title>
+	</head>
+	<body>
+		<h1>
+			egit上传github教程
+		</h1>
+		<ul>
+			<li>
+				1.安装egit插件
+			</li>
+			<li>
+				2.hbuilder中创建项目
+			</li>
+			<li>
+				3.项目右键“team”中“共享”项目
+			</li>
+			<li>
+				4.在项目的根目录中创建仓库文件夹；“create 仓库”
+			</li>
+			<li>
+				5.项目右键team中“commit"；最终commit and push即可
+			</li>
+			<li>
+				6.如果被拒绝上传，则采用“force update"
+			</li>
+		</ul>
+		<ul>
+			<li>
+				问题1：项目新建后，不能共享项目报错
+			</li>
+			<li>
+				问题2：每次commit都必须要进行输入密码的操作，很麻烦
+			</li>
+			<li>
+				问题3：每次commit都必须要输入地址，很麻烦
+			</li>
+		</ul>
+	</body>
+</html>
